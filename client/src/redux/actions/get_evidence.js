f295e940815c883cd0b73ec9b709f894776b4cb4
Charity A Hilton <Charity.Hilton@gtri.gatech.edu>
2019-10-21 23:17:44 -0400

    Update get_evidence.js

--- client/src/redux/actions/get_evidence.js
+++ client/src/redux/actions/get_evidence.js
@@ -1,59 +1,69 @@
 import axios from 'axios';
 
-import { ADD_EVIDENCE, ADD_EVIDENCE_SUCCESS, ADD_EVIDENCE_FAIL } from './types';
+import {
+    ADD_EVIDENCE,
+    ADD_EVIDENCE_SUCCESS,
+    ADD_EVIDENCE_FAIL
+} from './types';
 
 export const getEvidence = (patient, smart, form) => dispatch => {
-  const postData = {
-    reports: patient.docs,
-    fhir: smart
-  };
+    const postData = {
+        reports: patient.docs,
+        fhir: smart
+    };
 
-  console.log('ACCESS TOKEN: ', smart.state.tokenResponse.access_token);
+    console.log('ACCESS TOKEN: ', smart.state.tokenResponse.access_token);
 
-  // for (let query of form.evidence_bundles) {
+    let n = 0;
+    for (let query of form.evidence_bundles) {
 
-  for (let query of [
-    'wbc_hematologic_findings'
-  ]) {
+        //   for (let query of [
+        //     'wbc_hematologic_findings'
+        //   ]) {
 
-  // const query = form.evidence_bundles[0];
+        // const query = form.evidence_bundles[0];
 
-  // for
+        // for
 
-  const url = window._env_.REACT_APP_CLARITY_NLPAAS_URL + query;
-    // const url = process.env.REACT_APP_CLARITY_NLPAAS_URL_DEV + query;
+        const url = window._env_.REACT_APP_CLARITY_NLPAAS_URL + query;
+        // const url = process.env.REACT_APP_CLARITY_NLPAAS_URL_DEV + query;
+        setTimeout(() => {
+                dispatch({
+                    type: ADD_EVIDENCE,
+                    data: {
+                        nlpql: query
+                    }
+                });
 
-    dispatch({
-      type: ADD_EVIDENCE,
-      data: {
-        nlpql: query
-      }
-    });
+                axios
+                    .post(url, postData)
+                    .then(response => {
+                        dispatch({
+                            type: ADD_EVIDENCE_SUCCESS,
+                            data: {
+                                nlpql: query,
+                                results: response.data.filter(result => {
+                                    return (
+                                        result.hasOwnProperty('nlpql_feature') &&
+                                        result.nlpql_feature !== 'null'
+                                    );
+                                })
+                            }
+                        });
+                    })
+                    .catch(err => {
+                        dispatch({
+                            type: ADD_EVIDENCE_FAIL,
+                            data: {
+                                nlpql: query,
+                                results: [err]
+                            }
+                        });
+                    });
 
-    axios
-      .post(url, postData)
-      .then(response => {
-        dispatch({
-          type: ADD_EVIDENCE_SUCCESS,
-          data: {
-            nlpql: query,
-            results: response.data.filter(result => {
-              return (
-                result.hasOwnProperty('nlpql_feature') &&
-                result.nlpql_feature !== 'null'
-              );
-            })
-          }
-        });
-      })
-      .catch(err => {
-        dispatch({
-          type: ADD_EVIDENCE_FAIL,
-          data: {
-            nlpql: query,
-            results: [err]
-          }
-        });
-      });
-  }
+            },
+            (500 * n++)
+        )
+
+    }
 };
