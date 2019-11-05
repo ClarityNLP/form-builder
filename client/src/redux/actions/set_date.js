251da28b529a75671e7c340b477b437e095a62a9
Trey Schneider <trey@codeschneider.com>
2019-11-05 12:16:30 -0500

    cleanse v1 reminants

--- client/src/redux/actions/set_date.js
+++
@@ -1,8 +0,0 @@
-import { SET_DATE } from './types';
-
-export const setDate = date => dispatch => {
-  dispatch({
-    type: SET_DATE,
-    data: date
-  });
-};
--- client/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as appReducer } from './app_reducer';
-import { connectRouter } from 'connected-react-router';
-
-export default history =>
-  combineReducers({
-    app: appReducer,
-    router: connectRouter(history)
-  });
